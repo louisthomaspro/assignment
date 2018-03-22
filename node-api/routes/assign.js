797b8f24ef3422a42b48f8781d95974bd767b3e0
Louis <louisthomas.pro@gmail.com>
2018-03-22 14:40:13 +0100

    Comment assign.js

--- node-api/routes/assign.js
+++ node-api/routes/assign.js
@@ -5,6 +5,8 @@ var glpk = require('glpk.js/dist/glpk.min.js');
 var fs = require('fs');
 
 
+//======================= GLPK Functions =======================//
+
 function readMathprogFromString(tran, model, skip){
   var str = model;
   var pos = 0;
@@ -19,8 +21,6 @@ function readMathprogFromString(tran, model, skip){
     skip
   )
 }
-
-
 
 mathprog = function (model) {
   /* Create the problem */
@@ -50,32 +50,38 @@ mathprog = function (model) {
   //console.log(results);
   return results;
   //return [results, glpk.glp_mip_obj_val(lp)];
-
 };
 
 function glpk_print(string) {
   //console.log(string);
 }
 
+//===============================================================//
+
+
+// function for auto-change minPreference value
 function minPreferenceValue(value) {
   return 'param minPreferenceValue :='+value+';\n';
 }
 
+// Route "assign/"
 router.post('/',function(req,res,next){
   console.log('Récupération des parametres...');
   var params = JSON.parse(req.body.params);
   var array = JSON.parse(req.body.array);
   var priority = JSON.parse(req.body.priority);
 
-  // Inverted value of wishes (ex : 1 -> 5, 3 -> 2...) preference=nbProjet + 1 - preference
+  // inversion of the values of the wishes (ex : 1 -> 5, 3 -> 2...) --> preference = nbProjet + 1 - preference
   for (var etu in array) {
     for (var project in array[etu]) {
       array[etu][project] = params.nbProjects + 1 - array[etu][project];
     }
   }
 
-  // Generate text for .dat
-  // get params
+
+  //---- Generate text for .dat ----//
+
+  // Generate basic parameters
   console.log('Génération des parametres...');
   var dat = '';
   dat +=
@@ -86,9 +92,11 @@ router.post('/',function(req,res,next){
   'param ProjectsPerStudents :='+params.ProjectsPerStudents+ ';\n';
 
   // Generate wish table parameters
+  // ex : param preference : 1 2 3 4 5 :=
   dat += 'param preference : ';
-  for (var i=1;i<=params.nbProjects;i++) dat+=i+' ';  // ex : param preference : 1 2 3 4 5 :=
+  for (var i=1;i<=params.nbProjects;i++) dat+=i+' ';
   dat += ':=\n';
+
   // ex :
   // 1   1 5 3 2 4
   // 2   4 5 1 2 3
@@ -102,9 +110,9 @@ router.post('/',function(req,res,next){
   }
   dat+=";\n";
 
-
   // set priority wishes
-  var priority = priority[0]; // ["","","oui","",""]..
+  var priority = priority[0];
+  // ["","","oui","",""]..  to  [0,0,1,0,0]..
   for (var prio in priority) {
     if(priority[prio] === "") {
       priority[prio] = 0;
@@ -112,27 +120,27 @@ router.post('/',function(req,res,next){
       priority[prio] = 1;
     }
   }
-  // transformed in [0,0,1,0,0]..
 
-  // Create params
+  // Generate param for mandatoryProjects
+  // ex : param mandatoryProjects := 1 0 0 1 0;
   dat += 'param mandatoryProjects :=';
-
   dat+="\n";
   for (var prio in priority) {
     dat+=parseInt(prio)+1+'   ' + priority[prio];
     dat+="\n";
   }
-
-  // for (var prio in priority) dat+= ' ' + priority[prio];
   dat+=";\n";
-  // param mandatoryProjects := 1 0 0 1 0;
 
+  // Get path of the mod file
   var mod_url = __dirname + "/assign.mod";
+  // Read this file
   var mod = fs.readFileSync(mod_url).toString() + "\n";
 
-  // On va tester
+  // Test resolution
   console.log('Résolution en cours...');
   try {
+
+    //------- To change auto-minPreferenceValue -------//
     // var response;
     // for (var i=params.nbProjects;i>=1;i--) {
     //   console.log(i);
@@ -142,7 +150,10 @@ router.post('/',function(req,res,next){
     //     break;
     //   }
     // }
+
+    // Get all the model
     var model = mod+'data;\n'+dat+"end;";
+    // Resolve
     response = mathprog(model);
     console.log(response);
   }
@@ -150,10 +161,8 @@ router.post('/',function(req,res,next){
     console.log(error);
   }
 
-  //console.log(response);
   // response : [[0],[1],[0],[0],[0],[0],[1],[0],[0],[0],[1],[0],[0],[0],[0],[0],[0],[0],[0],[1],[0],[0],[0],[0],[1],[0],[1],[0],[0],[0],[0],[0],[0],[1],[0],[0],[0],[0],[1],[0],[0],[0],[1],[0],[0],[1],[0],[0],[0],[0],[0],[0],[1],[0],[0],[0],[0],[0],[0],[1],[4],[0]]
   // convert to this type [[1,2],[2,4],[3,1]....]
-
   var results = [];
   var index=0;
   if (response.length > 0) {
@@ -170,7 +179,7 @@ router.post('/',function(req,res,next){
   }
   console.log(results);
 
-  //console.log(results);
+  // return results
   res.json(results);
 });
 
